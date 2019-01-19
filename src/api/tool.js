25cfd62608bb98807ebb9b97dfb64c50f4f51a8d
Jalon <13560900514@139.com>
2019-01-19 16:15:18 +0800

    init

---
+++ src/api/tool.js
@@ -0,0 +1,27 @@
+import CryptoJS from '../assets/js/crypto-js';
+export default{
+     encrypt:(message, key)=> {
+        var keyHex = CryptoJS.enc.Utf8.parse(key);
+        var ivHex = CryptoJS.enc.Utf8.parse('asdewqrf');
+        var encrypted = CryptoJS.TripleDES.encrypt(message, keyHex, {
+            iv: ivHex,
+            mode: CryptoJS.mode.CBC,
+            padding: CryptoJS.pad.Pkcs7
+        });
+        return encrypted.toString();
+    },
+    sort_ASCII:(obj)=>{
+        let arr = new Array();
+        let num = 0;
+        for (let i in obj) {
+            arr[num] = i;
+            num++;
+        }
+        var sortArr = arr.sort();
+        let sign='';
+        for (let i in sortArr) {
+            sign=sign+sortArr[i]+'='+obj[sortArr[i]]+','
+        }
+        return sign.substring(0, sign.lastIndexOf(','));
+    }
+}
